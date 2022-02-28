9239f3577bcf23877d76088786991f1973cce684
Alex Trost <19617280+a-trost@users.noreply.github.com>
2022-02-27 19:58:40 -0500

    feat: Add catch-all route
    Adding a file that will catch every URL. We'll need to add some complexity to this file, but it seems to be the best way to handle what we have planned for our custom types.

---
+++ pages/[...slug].js
@@ -0,0 +1,45 @@
+import Head from "next/head";
+import { createClient } from "../prismicio";
+
+export default function Event({ page }) {
+  return (
+    <div>
+      <Head>
+        <title>{page.metaTitle}</title>
+        <meta name="description" content={page.metaDescription} />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <div className="layout">
+          <h1>{page.eventTitle}</h1>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export async function getStaticPaths(props) {
+  const client = createClient();
+  const events = await client.getAllByType("event");
+
+  return {
+    paths: events.map(({ uid }) => ({ params: { slug: [uid] } })) || [],
+  };
+}
+export async function getStaticProps({ params }) {
+  const uid = params.slug[0];
+
+  const client = createClient();
+  const doc = await client.getByUID("event", uid);
+
+  const page = {
+    ...doc.data,
+    slugs: doc.slugs,
+    uid: doc.uid,
+    type: doc.type,
+  };
+
+  return {
+    props: { page },
+  };
+}
